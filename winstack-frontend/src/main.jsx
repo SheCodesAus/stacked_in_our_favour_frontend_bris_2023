e360a5f0c5b600b850225ec623e0fad5a99d5fad
Claire Delosa <clair650@gmail.com>
2023-10-14 12:18:40 +1000

    Creating router in main and ReactDOM?

--- winstack-frontend/src/main.jsx
+++ winstack-frontend/src/main.jsx
@@ -1,26 +1,33 @@
+import React from "react";
+import { ReactDOM } from "react-dom/client";
+import { createBrowserRouter, RouterProvider} from "react-router-dom";
+
 import HPLoggedOut from './pages/HomePage/HPLoggedOut'
 // import App from './App.jsx'
 // import './index.css'
 
-// ReactDOM.createRoot(document.getElementById('root')).render(
-//   <React.StrictMode>
-    
-//   </React.StrictMode>,
-// )
 
-function HomeTest(){
-  return (
-      <HPLoggedOut/>
-  );
-}
 
-export default HomeTest
-// const router = createBrowserRouter([
-//   {
-//     path: '/',
-//     element: <NavBar />,
-//     children: [
-//       { path: '/', element: <HPLoggedOut/>}
-//     ],
-//   },
-// ]);
+// function HomeTest(){
+//   return (
+//       <HPLoggedOut/>
+//   );
+// }
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <NavBar />,
+    children: [
+      { path: '/', element: <HPLoggedOut />}
+    ],
+  },
+]); 
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <RouterProvider router={router} />
+  </React.StrictMode>,
+)
+
+// export default HomeTest;
+
