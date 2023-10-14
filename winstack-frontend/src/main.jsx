2d68f4ac99ab7e18d04633d5fa47533b7481ac1a
Pauline Segi <p.segi@outlook.com>
2023-10-14 12:22:55 +1000

    committing NavBar changes

--- winstack-frontend/src/main.jsx
+++ winstack-frontend/src/main.jsx
@@ -1,8 +1,31 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { createBrowserRouter, RouterProvider } from "react-router-dom";
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-)
+import HomePage from "./pages/HomePage.jsx";
+
+import NavBar from "./components/NavBar.jsx";
+
+
+
+const router = createBrowserRouter([
+    {
+        path: "/",
+        element: <NavBar />,
+        children: [
+                { path: "/", element: <HomePage /> },
+                { path: "/register", element: <LoginPage />},
+                { path: "/profile", element: <ProfilePage />},
+                { path: "/event", element: <EventPage />},
+                { path: "/stickyNotes", element: <StickyNotesPage />},
+            ],
+    },
+]);
+
+ReactDOM.createRoot(document.getElementById("root")).render(
+    <React.StrictMode>
+        <AuthProvider>
+            <RouterProvider router={router} />
+        </AuthProvider>
+    </React.StrictMode>
+    );
--- winstack-frontend/src/pages/HomePage.jsx
+++ winstack-frontend/src/pages/HomePage.jsx
@@ -3,9 +3,12 @@ import "../components/NavBar.css"
 import "./HomePage.css";
 
 function HomePage() {
-
+    return (
+        <div>
+            <h3>Home Page</h3>
+        </div>
+    )
 }
-
 
 
 export default HomePage;
