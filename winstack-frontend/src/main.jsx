ad145a9de6872e2a9db7168dd75ec3bed87a932c
Claire Delosa <clair650@gmail.com>
2023-10-14 11:51:48 +1000

    Testing home page.

--- winstack-frontend/src/main.jsx
+++ winstack-frontend/src/main.jsx
@@ -1,10 +1,26 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
+import HPLoggedOut from './pages/HomePage/HPLoggedOut'
 // import App from './App.jsx'
 // import './index.css'
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    {/* <App /> */}
-  </React.StrictMode>,
-)
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+    
+//   </React.StrictMode>,
+// )
+
+function HomeTest(){
+  return (
+      <HPLoggedOut/>
+  );
+}
+
+export default HomeTest
+// const router = createBrowserRouter([
+//   {
+//     path: '/',
+//     element: <NavBar />,
+//     children: [
+//       { path: '/', element: <HPLoggedOut/>}
+//     ],
+//   },
+// ]);
--- winstack-frontend/src/pages/HomePage/HPLoggedOut.jsx
+++ winstack-frontend/src/pages/HomePage/HPLoggedOut.jsx
@@ -1,9 +1,7 @@
 function HPLoggedOut(){
-
         return (
             <h1>hello</h1>
         );
-
 }
 
 export default HPLoggedOut;
