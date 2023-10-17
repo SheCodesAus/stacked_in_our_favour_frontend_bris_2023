b2b1ab145ea3fb64212ba3b1f203efbc08d77e13
Claire Delosa <clair650@gmail.com>
2023-10-17 18:30:28 +1000

    made changes to images to see if helps with deploy

--- src/components/PromoteBanner.jsx
+++ src/components/PromoteBanner.jsx
@@ -1,24 +1,23 @@
-import Celebrate from "../img/celebrate.png";
-import Engage from "../img/Engage.png";
-import Explore from "../img/Explore.png";
+// import Celebrate from "../img/celebrate.png";
+// import Engage from "../img/Engage.png";
 import "./PromoteBanner.css"
 
 function PromoteBanner() {
     return (
         <>
-            <ul class="banner-block">
-                <div class="block-item">
-                    <img src={Celebrate} alt="Icon of confetti." />
+            <ul className="banner-block">
+                <div className="block-item">
+                    {/* <img src={Celebrate} alt="Icon of confetti." /> */}
                     <h3>Celebrate Wins</h3>
                     <p>Share your wins from workshops with your peers and leaders.</p>
                 </div>
-                <div class="block-item">
-                    <img src={Engage} alt="Icon of three people and a heart behind them." />
+                <div className="block-item">
+                    {/* <img src={Engage} alt="Icon of three people and a heart behind them." /> */}
                     <h3>Engage With Peers</h3>
                     <p>Contribute and view peers thoughts during and after workshops.</p>
                 </div>
-                <div class="block-item">
-                    <img src={Explore} alt="Icon of a graduation cap." />
+                <div className="block-item">
+                    {/* <img src={Explore} alt="Icon of a graduation cap." /> */}
                     <h3>Explore Workshops</h3>
                     <p>View past, current and upcoming workshops hosted within the SheCodes network.</p>
                 </div>
