506c71f5d9d22a4e0ddf4ff2b98fa25cba7b11ea
Claire <clair650@gmail.com>
2023-10-21 19:09:17 +1000

    made changes to styling that was erased in merge.

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -4,72 +4,63 @@ import postLogin from "../api/postLogin";
 import useAuth from "../hooks/use-auth";
 import "./Form.css";
 
-function LoginForm() {    
+function LoginForm() {
     const navigate = useNavigate();
-    const {auth, setAuth} = useAuth();    
-    
-    const [credentials, setCredentials] = useState({        
-        username: "",        
-        password: "",    
-    });    
-    
-    const handleChange = (event) => {        
-        const { id, value } = event.target;        
-        setCredentials((prevCredentials) => ({            
-            ...prevCredentials,            
-            [id]: value,        
-        }));    
-    };    
-    
-    const handleSubmit = (event) => {        
-        event.preventDefault();        
-        if (credentials.username && credentials.password) {            
-            postLogin(                
-                credentials.username,                
-                credentials.password            
-                ).then((response) => {                
-                    window.localStorage.setItem("token", response.token);
-                    setAuth({                   
-                        token: response.token,               
-                    });                
-                    navigate("/");            
-                });        
-            }    
-        };
+    const { auth, setAuth } = useAuth();
+
+    const [credentials, setCredentials] = useState({
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
+        }));
+    };
+
+    const handleSubmit = (event) => {
+        event.preventDefault();
+        if (credentials.username && credentials.password) {
+            postLogin(
+                credentials.username,
+                credentials.password
+            ).then((response) => {
+                window.localStorage.setItem("token", response.token);
+                setAuth({
+                    token: response.token,
+                });
+                navigate("/");
+            });
+        }
+    };
 
     return (
         <form>
             <h1>Login</h1>
-            <div className="text-field-style">
-                {/* <label htmlFor="email" className="row">Email</label>
-                        <input 
-                            type="email" 
-                            id="email" 
-                            placeholder="Enter email"
-                            onChange={handleChange}
-                        />        */}
-                </div>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                        <input 
-                            type="text" 
-                            id="username" 
-                            placeholder="Enter username"
-                            onChange={handleChange}
-                        />  
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input 
-                        type="password" 
-                        id="password" 
-                        placeholder="Enter password" 
-                        onChange={handleChange}
-                    />      
-                </div>
-                <div className="button-style">
-                <button type="submit" onClick={handleSubmit}><span>Login</span></button>  
-                </div>
+            <div className="input-styling">
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Enter username"
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="input-styling">
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Enter password"
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="input-styling">
+                <button type="submit" onClick={handleSubmit}><span>Login</span></button>
+            </div>
         </form>
     );
 }
--- src/components/RegisterForm.jsx
+++ src/components/RegisterForm.jsx
@@ -4,71 +4,71 @@ import postRegister from "../api/postRegister";  // Change this to your registra
 import useAuth from "../hooks/use-auth.js";
 import "./Form.css";
 
-function RegisterForm() {    
+function RegisterForm() {
     const navigate = useNavigate();
-    const {auth, setAuth} = useAuth();    
+    const { auth, setAuth } = useAuth();
     const [errorMessage, setErrorMessage] = useState("");  // State to hold error messages
-    
-    const [credentials, setCredentials] = useState({  
-        email: "",      
-        username: "",        
-        password: "",    
-    });    
-    
-    const handleChange = (event) => {        
-        const { id, value } = event.target;        
-        setCredentials((prevCredentials) => ({            
-            ...prevCredentials,            
-            [id]: value,        
+
+    const [credentials, setCredentials] = useState({
+        email: "",
+        username: "",
+        password: "",
+    });
+
+    const handleChange = (event) => {
+        const { id, value } = event.target;
+        setCredentials((prevCredentials) => ({
+            ...prevCredentials,
+            [id]: value,
         }));
         setErrorMessage("");  // Clear error messages when user types    
-    };    
-    
-    const handleSubmit = async (event) => {        
-        event.preventDefault();        
-        if (credentials.email && credentials.username && credentials.password) {            
+    };
+
+    const handleSubmit = async (event) => {
+        event.preventDefault();
+        if (credentials.email && credentials.username && credentials.password) {
             try {
-                const response = await postRegister(                
+                const response = await postRegister(
                     credentials.email,
-                    credentials.username,                
-                    credentials.password            
+                    credentials.username,
+                    credentials.password
                 );
                 window.localStorage.setItem("token", response.token);
-                setAuth({                   
-                    token: response.token,               
-                });                
+                setAuth({
+                    token: response.token,
+                });
                 navigate("/");
-            } catch(error) {
+            } catch (error) {
                 setErrorMessage(error.message);
                 console.error("Registration error:", error.message);
-            }     
-        }    
+            }
+        }
     };
 
     return (
         <form onSubmit={handleSubmit}>
-            <div>
-                <h1>Register</h1>     
-                </div>
-                <div>
-                    <label htmlFor="username">Username:</label>
-                        <input 
-                            type="text" 
-                            id="username" 
-                            placeholder="Enter username"
-                            onChange={handleChange}
-                        />  
-                </div>
-                <div>
-                    <label htmlFor="password">Password:</label>
-                    <input 
-                        type="password" 
-                        id="password" 
-                        placeholder="Enter password" 
-                    />      
-                </div>
-                <button type="submit">Register</button>
-                <a href="/login">Login</a>    
+            <h1>Register</h1>
+            <div className="input-styling">
+                <label htmlFor="username">Username</label>
+                <input
+                    type="text"
+                    id="username"
+                    placeholder="Enter username"
+                    onChange={handleChange}
+                />
+            </div>
+            <div className="input-styling">
+                <label htmlFor="password">Password</label>
+                <input
+                    type="password"
+                    id="password"
+                    placeholder="Enter password"
+                />
+            </div>
+            <div className="input-styling">
+                <button type="submit"><span>Register</span></button>
+            </div>
+            <a href="/login" className="style-a">Login</a>
         </form>
     );
 }
