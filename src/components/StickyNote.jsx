84e25fdda3ca14b3d89b31270b34a3fd69cfd887
emart55 <emilymartin_55@outlook.com>
2023-10-23 23:39:14 +1000

    Changes to formatting of sticky notes

--- src/components/StickyNote.jsx
+++ src/components/StickyNote.jsx
@@ -3,13 +3,16 @@ import "./StickyNote.css";
 
 // Sticky Note component
 function StickyNote(props) {
-    const { stickyData }= props;
-    
+    const { stickyData } = props;
+    const colors = ["pink", "yellow", "green", "plum"]; // add more if needed
+    const randomColor = colors[Math.floor(Math.random() * colors.length)];
+
     return (
-        <div className="sticky-note">
+        <div className={`sticky-note ${randomColor}`}>
             <h2>{stickyData.noteText}</h2>
             <h2>By {stickyData.authorID}</h2>    
-        </div>  
-    );}
+        </div>
+    );
+}
     
 export default StickyNote;
