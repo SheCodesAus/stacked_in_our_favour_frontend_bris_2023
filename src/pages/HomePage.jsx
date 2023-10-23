10aee33f63ce4b4d416e609ec24de302520c4e99
Pauline Segi <p.segi@outlook.com>
2023-10-23 19:50:24 +1000

    completed responsiveness for home page

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from "react";
-import "../components/NavBar.css"
+import "../components/NavBar.css";
 import PromoteBanner from "../components/PromoteBanner";
 import stickyImage from "../assets/stickyImage.svg";
 
@@ -16,15 +16,15 @@ function HomePage() {
 
     useEffect(() => {
         const handleResize = () => {
-            setIsMobileView(window.innerWidth < 768);
+            setIsMobileView(window.innerWidth <= 768); // Adjust the breakpoint to 1100px
         };
 
         handleResize();
 
-        window.addEventListener('resize', handleResize);
+        window.addEventListener("resize", handleResize);
 
         return () => {
-            window.removeEventListener('resize', handleResize);
+            window.removeEventListener("resize", handleResize);
         };
     }, []);
 
@@ -32,16 +32,30 @@ function HomePage() {
         <>
             <div class="home-block">
                 <div class="home-block-text">
-                    <h1>Celebrate WINS during your <span style={{color: PurpleText.color}}>SHE</span><span style={{color: YellowText.color}}>&#123;CODES&#125;</span> workshops.</h1>
-                    <h2 style={{ marginTop: '24px' }}>Share successes with your peers and mentors before, during and after workshop events! </h2>
+                    <h1>
+                        Celebrate WINS during your{" "}
+                        <span style={{ color: PurpleText.color }}>SHE</span>
+                        <span style={{ color: YellowText.color }}>&#123;CODES&#125;</span> workshops.
+                    </h1>
+                    <h2 style={{ marginTop: "24px" }}>
+                        Share successes with your peers and mentors before, during and after workshop events!
+                    </h2>
                 </div>
-                <div class={`home-block-image ${isMobileView ? "mobile-view" : "desktop-view"}`}>
-                    <img src={stickyImage} alt="Illustration of people posting sticky notes onto a board." />
+                <div
+                    class={`home-block-image ${
+                        isMobileView ? "mobile-view" : "desktop-view"
+                    }`}
+                >
+                    <img
+                        src={stickyImage}
+                        alt="Illustration of people posting sticky notes onto a board."
+                        class={`shrink-image ${isMobileView ? "disappear" : ""}`}
+                    />
                 </div>
             </div>
             <PromoteBanner isMobileView={isMobileView} />
         </>
-    )
+    );
 }
 
 export default HomePage;
